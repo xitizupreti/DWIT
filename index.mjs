495a226ac84a6028b7b3996c49c6163f6290c6ce
Kshitiz Upreti <upreti.kshitiz.ku@gmail.com>
2023-12-23 15:44:25 +0545

    Day5 12/22 {innerHTML}

--- index.mjs
+++ index.mjs
@@ -40,48 +40,66 @@ function dv(a = 10, b = 20) {
 function add() {
   let a = prompt("Enter 1st number:");
   let b = prompt("Enter 2nd number:");
+  let dynamicbutton = document.getElementById("add");
+  dynamicbutton.innerHTML = `${a}+${b}=${Number(a) + Number(b)}`;
   alert(Number(a) + Number(b));
 }
 function sub() {
   let a = prompt("Enter 1st number:");
   let b = prompt("Enter 2nd number:");
+  let dynamicbutton = document.getElementById("sub");
+  dynamicbutton.innerHTML = `${a}-${b}=${Number(a) - Number(b)}`;
   alert(Number(a) - Number(b));
 }
 function mul() {
   let a = prompt("Enter 1st number:");
   let b = prompt("Enter 2nd number:");
+  let dynamicbutton = document.getElementById("mul");
+  dynamicbutton.innerHTML = `${a}x${b}=${Number(a) * Number(b)}`;
   alert(Number(a) * Number(b));
 }
 function div() {
   let a = prompt("Enter 1st number:");
   let b = prompt("Enter 2nd number:");
+  let dynamicbutton = document.getElementById("div");
+  dynamicbutton.innerHTML = `${a}/${b}=${Number(a) / Number(b)}`;
   alert(Number(a) / Number(b));
 }
 
 function isGreaterThan18eo() {
   let a = prompt("Enter age or random number");
-  let dynamicbutton = document.getElementById("dynamic");
+  let dynamicbutton = document.getElementById("eo");
   if (a >= 18) {
     if (a % 2 === 1) {
       dynamicbutton.innerHTML = "True && Odd";
+      alert("True && Odd");
     } else {
       dynamicbutton.innerHTML = "True && Even";
+      alert("True && Even");
     }
     /* cSpell:disable */
   } else if (a < 18) {
     if (a % 2 === 1) {
       dynamicbutton.innerHTML = "False && Odd";
+      alert("False && Odd");
     } else {
       dynamicbutton.innerHTML = "False && Even";
+      alert("False && Even");
     }
   } else {
     dynamicbutton.innerHTML = "Error Age";
+    alert("Error Age");
   }
 }
 
-function avg3() {
+function avg() {
   let a = prompt("Enter 1st number:");
   let b = prompt("Enter 2nd number:");
   let c = prompt("Enter 3rd number:");
-  alert((Number(a) + Number(b) + Number(b)) / 3);
+  let dynamicbutton = document.getElementById("avg");
+  dynamicbutton.innerHTML = `${a},${b},${c}=${(
+    (Number(a) + Number(b) + Number(b)) /
+    3
+  ).toFixed(2)}`;
+  alert(((Number(a) + Number(b) + Number(b)) / 3).toFixed(2));
 }
