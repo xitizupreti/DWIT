d73d7dc235d2a30da80f7999e4ac2762790b915e
Kshitiz Upreti <upreti.kshitiz.ku@gmail.com>
2024-01-03 17:00:32 +0545

    Destructing, Spread, ObjectKeyValues

---
+++ day11-0103.mjs
@@ -0,0 +1,44 @@
+// make a arrow function that takes input as word "my name is *" and it must conver the input to camelCase like "My Name Is x"
+// const toCamelCase = (sentence) => {
+//   return sentence
+//     .toLowerCase()
+//     .split(" ")
+//     .map((word) => word[0].toUpperCase() + word.slice(1))
+//     .join(" ");
+// };
+
+// const inputSentence = "my name is xitiz";
+// const camelCaseResult = toCamelCase(inputSentence);
+// console.log(camelCaseResult);
+
+// let error = new Error("404");
+// throw error;
+
+// Array Destructing
+// let [a, b, c] = [1, 2, 3];
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+// let info = { name: "xitiz", age: 23 };
+// console.log(info.age);
+// Object Destructing
+// let { name, isMarried, age } = { name: "xitiz", age: 23, isMarried: false };
+// console.log(name);
+// console.log(age);
+// console.log(isMarried);
+
+// spread operator
+// let b1 = ["apple", "ball", "cat"];
+// let b2 = ["mango", "orange"];
+// let b3 = [...b1, "a", "b"];
+// let b4 = [...b1, "c", b2, "d"];
+// console.log(b3);
+// console.log(b4);
+
+let obj1 = { name: "xitiz", age: 23 };
+let obj2 = { address: "baneshwor" };
+let obj3 = { isMarried: false, ...obj1, ...obj2 };
+console.log(obj3);
+console.log(Object.keys(obj1));
+console.log(Object.values(obj2));
